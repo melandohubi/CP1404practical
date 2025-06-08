f039448592ae74a14684ca67420f7451e00aecf2
melandohubi <hubimelando285@gmail.com>
2025-06-08 19:43:37 +0800

    password_checker.py

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -1,8 +1,66 @@
-MIN_LENGTH = 2
+"""MIN_LENGTH = 2
 MAX_LENGTH = 6
 IS_SPECIAL_CHARACTER_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
+function main()
+    print("Please enter a valid password")
+    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH, "characters, and contain:")
+    print("    1 or more uppercase characters")
+    print("    1 or more lowercase characters")
+    print("    1 or more numbers")
+    if IS_SPECIAL_CHARACTER_REQUIRED == True:
+        print("    and 1 or more special characters:", SPECIAL_CHARACTERS)
+
+    password = input("> ")
+    while is_valid_password(password) == False:
+        print("Invalid password!")
+        password = input("> ")
+
+    print("Your", len(password), "-character password is valid:", password)
+end function
+
+
+function is_valid_password(password)
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+
+    number_of_lower = 0
+    number_of_upper = 0
+    number_of_digit = 0
+    number_of_special = 0
+
+    for character in password:
+        if character.islower() == True:
+            number_of_lower += 1
+        elif character.isupper() == True:
+            number_of_upper += 1
+        elif character.isdigit() == True:
+            number_of_digit += 1
+        elif character in SPECIAL_CHARACTERS:
+            number_of_special += 1
+
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
+
+    if IS_SPECIAL_CHARACTER_REQUIRED == True and number_of_special == 0:
+        return False
+
+    return True
+end function
+
+main()
+"""
+
+"""
+CP1404/CP5632 - Practical
+Password checker completed code
+"""
+
+MIN_LENGTH = 2
+MAX_LENGTH = 6
+IS_SPECIAL_CHARACTER_REQUIRED = False
+SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 def main():
     """Program to get and check a user's password."""
@@ -12,20 +70,17 @@ def main():
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
     if IS_SPECIAL_CHARACTER_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
-
+        print(f"\tand 1 or more special characters: {SPECIAL_CHARACTERS}")
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
-
     print(f"Your {len(password)}-character password is valid: {password}")
 
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
 
-    # Check length
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
@@ -41,7 +96,7 @@ def is_valid_password(password):
             number_of_upper += 1
         elif character.isdigit():
             number_of_digit += 1
-        elif IS_SPECIAL_CHARACTER_REQUIRED and character in SPECIAL_CHARACTERS:
+        elif character in SPECIAL_CHARACTERS:
             number_of_special += 1
 
     if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
