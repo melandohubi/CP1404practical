75f80b9e5788f1bca35e69f6b6060a9e6f185c94
melandohubi <hubimelando285@gmail.com>
2025-07-06 18:45:56 +0800

    programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,34 +1,44 @@
-class ProgrammingLanguage:
-    """Represent a programming language."""
+"""
+Estimate: 15 minutes
+Start time: 2025-07-06 18:30
+Actual time taken: ___ minutes (fill this in after testing)
+"""
 
+"""CLASS ProgrammingLanguage
+
+    FUNCTION __init__(name, typing, reflection, year)
+        SET self.name = name
+        SET self.typing = typing
+        SET self.reflection = reflection
+        SET self.year = year
+    END FUNCTION
+
+    FUNCTION is_dynamic()
+        IF self.typing == "Dynamic" THEN
+            RETURN True
+        ELSE
+            RETURN False
+        END IF
+    END FUNCTION
+
+    FUNCTION __str__()
+        RETURN formatted string:
+            self.name + ", " + self.typing + " Typing, Reflection=" + self.reflection
+            + ", First appeared in " + self.year
+    END FUNCTION
+
+END CLASS
+"""
+
+class ProgrammingLanguage:
     def __init__(self, name, typing, reflection, year):
-        """Initialise a ProgrammingLanguage instance."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
-        """Return True if the programming language is dynamically typed."""
-        return self.typing.lower() == "dynamic"
+        return self.typing == "Dynamic"
 
-
-# Example usage
-def main():
-    # Creating instances of ProgrammingLanguage
-    languages = [
-        ProgrammingLanguage("Java", "Static", "Yes", 1995),
-        ProgrammingLanguage("C++", "Static", "No", 1983),
-        ProgrammingLanguage("Python", "Dynamic", "Yes", 1991),
-        ProgrammingLanguage("Visual Basic", "Static", "No", 1991),
-        ProgrammingLanguage("Ruby", "Dynamic", "Yes", 1995),
-    ]
-
-    # Displaying information about each language
-    for language in languages:
-        dynamic_status = "dynamically typed" if language.is_dynamic() else "statically typed"
-        print(f"{language.name} is {dynamic_status} and was created in {language.year}.")
-
-
-if __name__ == "__main__":
-    main()
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
